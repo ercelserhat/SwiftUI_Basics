48817e58f8632766ff588eba05e8ea782b200626
ercelserhat <ercelserhat@gmail.com>
2023-10-06 00:05:15 +0300

    Frame

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -9,20 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Rectangle().fill(.gray).frame(width: 400, height: 400)
-            VStack(alignment:.leading, spacing: 50){
-                Rectangle().fill(.red).frame(width: 100, height: 100)
-                Spacer()
-                Rectangle().fill(.green).frame(width: 80, height: 80)
-                Rectangle().fill(.blue).frame(width: 50, height: 50)
-                HStack(alignment: .bottom, spacing: 50){
-                    Rectangle().fill(.cyan).frame(width: 100, height: 100)
-                    Spacer().frame(width: 20, height: 20)
-                    Rectangle().fill(.indigo).frame(width: 80, height: 80)
-                    Rectangle().fill(.mint).frame(width: 50, height: 50)
-                }
-            }
+        HStack{
+            Rectangle().fill(.yellow).frame(maxWidth: 100, maxHeight: .infinity)
+            Rectangle().fill(.pink).frame(width: 100, height: 200)
         }
     }
 }
