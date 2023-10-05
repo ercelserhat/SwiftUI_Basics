4b56776a8599b52b713fcbb9f61a5b3e6215453e
ercelserhat <ercelserhat@gmail.com>
2023-10-05 23:41:31 +0300

    Hello World

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Serhat on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
