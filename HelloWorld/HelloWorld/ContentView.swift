6747a788fed31862ec63b342fa7b14b9255770a0
ercelserhat <ercelserhat@gmail.com>
2023-10-05 23:50:36 +0300

    ZStack, VStack, HStack

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle().fill(.gray).frame(width: 400, height: 400)
+            VStack{
+                Rectangle().fill(.red).frame(width: 100, height: 100)
+                Rectangle().fill(.green).frame(width: 80, height: 80)
+                Rectangle().fill(.blue).frame(width: 50, height: 50)
+                HStack{
+                    Rectangle().fill(.cyan).frame(width: 100, height: 100)
+                    Rectangle().fill(.indigo).frame(width: 80, height: 80)
+                    Rectangle().fill(.mint).frame(width: 50, height: 50)
+                }
+            }
         }
-        .padding()
     }
 }
 
