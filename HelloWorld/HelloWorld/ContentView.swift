58201a0d6b0876febbbfd274cad5cec50dec4419
ercelserhat <ercelserhat@gmail.com>
2023-10-05 23:53:08 +0300

    Alignment

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -11,11 +11,11 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Rectangle().fill(.gray).frame(width: 400, height: 400)
-            VStack{
+            VStack(alignment:.leading){
                 Rectangle().fill(.red).frame(width: 100, height: 100)
                 Rectangle().fill(.green).frame(width: 80, height: 80)
                 Rectangle().fill(.blue).frame(width: 50, height: 50)
-                HStack{
+                HStack(alignment: .bottom){
                     Rectangle().fill(.cyan).frame(width: 100, height: 100)
                     Rectangle().fill(.indigo).frame(width: 80, height: 80)
                     Rectangle().fill(.mint).frame(width: 50, height: 50)
