e415225aecc8fec2a81fdc5c5981e23a245c214e
ercelserhat <ercelserhat@gmail.com>
2023-10-06 00:00:12 +0300

    Spacer

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -13,10 +13,12 @@ struct ContentView: View {
             Rectangle().fill(.gray).frame(width: 400, height: 400)
             VStack(alignment:.leading, spacing: 50){
                 Rectangle().fill(.red).frame(width: 100, height: 100)
+                Spacer()
                 Rectangle().fill(.green).frame(width: 80, height: 80)
                 Rectangle().fill(.blue).frame(width: 50, height: 50)
                 HStack(alignment: .bottom, spacing: 50){
                     Rectangle().fill(.cyan).frame(width: 100, height: 100)
+                    Spacer().frame(width: 20, height: 20)
                     Rectangle().fill(.indigo).frame(width: 80, height: 80)
                     Rectangle().fill(.mint).frame(width: 50, height: 50)
                 }
