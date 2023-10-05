afc579534f68dd74c05d155caa46bebd97fb5bdd
ercelserhat <ercelserhat@gmail.com>
2023-10-06 00:10:22 +0300

    Custom Widgets

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            Rectangle().fill(.yellow).frame(maxWidth: 100, maxHeight: .infinity)
-            Rectangle().fill(.pink).frame(width: 100, height: 200)
+            SariKare()
+            PembeKare()
+            Metin(icerik: "Merhaba", yaziBoyutu: 25)
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct SariKare: View{
+    var body: some View{
+        Rectangle().fill(.yellow).frame(maxWidth: 100, maxHeight: .infinity)
+    }
+}
+
+struct PembeKare: View{
+    var body: some View{
+        Rectangle().fill(.pink).frame(width: 100, height: 200)
+    }
+}
+
+struct Metin: View{
+    var icerik = ""
+    var yaziBoyutu = 0
+    var body: some View{
+        Text(icerik).font(.system(size: CGFloat(yaziBoyutu)))
+    }
 }
