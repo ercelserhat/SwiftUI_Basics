2859636cbfc1afea2571857eabec3285cb20a484
ercelserhat <ercelserhat@gmail.com>
2023-10-06 00:26:52 +0300

    Add new page

---
+++ HelloWorld/HelloWorld/NewPage.swift
@@ -0,0 +1,18 @@
+//
+//  NewPage.swift
+//  HelloWorld
+//
+//  Created by Serhat on 6.10.2023.
+//
+
+import SwiftUI
+
+struct NewPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPage()
+}
