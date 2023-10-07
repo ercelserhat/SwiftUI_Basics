7244a6c4ffd2dabd9907b161ca55a447a9ace276
ercelserhat <ercelserhat@gmail.com>
2023-10-07 16:49:37 +0300

    Data transfer with OOP

---
+++ Segue/Segue/Kisiler.swift
@@ -0,0 +1,18 @@
+//
+//  Kisiler.swift
+//  Segue
+//
+//  Created by Serhat on 7.10.2023.
+//
+
+import Foundation
+
+class Kisiler{
+    var isim: String?
+    var yas: Int?
+    
+    init(isim: String? = nil, yas: Int? = nil) {
+        self.isim = isim
+        self.yas = yas
+    }
+}
