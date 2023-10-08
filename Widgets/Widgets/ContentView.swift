e043aaff02b09866b9f1e8471267c85a161ca4e0
ercelserhat <ercelserhat@gmail.com>
2023-10-08 23:50:33 +0300

    Widgets - Stepper

--- Widgets/Widgets/ContentView.swift
+++ Widgets/Widgets/ContentView.swift
@@ -9,37 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var segmentedDurum = 0
+    @State private var stepperDurum = 0
     
     var body: some View {
         VStack{
-            Picker("", selection: $segmentedDurum){
-                Text("Yemekler").tag(0)
-                Text("Tatlılar").tag(1)
-                Text("İçecekler").tag(2)
-            }.onChange(of: segmentedDurum) { oldValue, newValue in
-                print("Segmented: \(newValue)")
-            }
-            Picker("", selection: $segmentedDurum){
-                Text("Yemekler").tag(0)
-                Text("Tatlılar").tag(1)
-                Text("İçecekler").tag(2)
-            }.pickerStyle(.inline)
-            Picker("", selection: $segmentedDurum){
-                Text("Yemekler").tag(0)
-                Text("Tatlılar").tag(1)
-                Text("İçecekler").tag(2)
-            }.pickerStyle(.palette)
-            Picker("", selection: $segmentedDurum){
-                Text("Yemekler").tag(0)
-                Text("Tatlılar").tag(1)
-                Text("İçecekler").tag(2)
-            }.pickerStyle(.wheel)
-            Picker("", selection: $segmentedDurum){
-                Text("Yemekler").tag(0)
-                Text("Tatlılar").tag(1)
-                Text("İçecekler").tag(2)
-            }.pickerStyle(.segmented)
+            Stepper("Stepper", value: $stepperDurum, in: 0...10)
+                .padding()
+            Text("Sonuç: \(stepperDurum)")
         }
         .padding()
     }
