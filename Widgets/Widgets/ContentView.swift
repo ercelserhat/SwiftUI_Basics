5f990c34b703e55721fecaedc7de6b15b2abe505
ercelserhat <ercelserhat@gmail.com>
2023-10-08 23:48:26 +0300

    Widgets - Segmented

--- Widgets/Widgets/ContentView.swift
+++ Widgets/Widgets/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var switchDurum = false
+    @State private var segmentedDurum = 0
     
     var body: some View {
-        VStack (spacing: 20){
-            Toggle("Switch", isOn: $switchDurum)
-                .onChange(of: switchDurum, { oldValue, newValue in
-                    print("Switch Durum: \(newValue)")
-                })
-                .toggleStyle(SwitchToggleStyle(tint: .red))
-                .padding()
+        VStack{
+            Picker("", selection: $segmentedDurum){
+                Text("Yemekler").tag(0)
+                Text("Tatlılar").tag(1)
+                Text("İçecekler").tag(2)
+            }.onChange(of: segmentedDurum) { oldValue, newValue in
+                print("Segmented: \(newValue)")
+            }
+            Picker("", selection: $segmentedDurum){
+                Text("Yemekler").tag(0)
+                Text("Tatlılar").tag(1)
+                Text("İçecekler").tag(2)
+            }.pickerStyle(.inline)
+            Picker("", selection: $segmentedDurum){
+                Text("Yemekler").tag(0)
+                Text("Tatlılar").tag(1)
+                Text("İçecekler").tag(2)
+            }.pickerStyle(.palette)
+            Picker("", selection: $segmentedDurum){
+                Text("Yemekler").tag(0)
+                Text("Tatlılar").tag(1)
+                Text("İçecekler").tag(2)
+            }.pickerStyle(.wheel)
+            Picker("", selection: $segmentedDurum){
+                Text("Yemekler").tag(0)
+                Text("Tatlılar").tag(1)
+                Text("İçecekler").tag(2)
+            }.pickerStyle(.segmented)
         }
         .padding()
     }
