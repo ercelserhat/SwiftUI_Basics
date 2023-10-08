7f56715b99cdf2d25e11f85420873e93d9dc89b3
ercelserhat <ercelserhat@gmail.com>
2023-10-08 22:24:09 +0300

    NavigationStack and ToolBar

---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Serhat on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
