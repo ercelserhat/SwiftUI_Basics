01687c0113019b8b62e442ef176a04c7432d8b55
ercelserhat <ercelserhat@gmail.com>
2023-10-08 23:01:51 +0300

    Widgets - Text, TextField, Button Customizing

--- Widgets/Widgets/ContentView.swift
+++ Widgets/Widgets/ContentView.swift
@@ -15,14 +15,33 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Gelen Veri: \(alinanVeri)")
+                .font(.system(size: 25))
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .bold()
             TextField("Veri giriniz", text: $textField)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .textFieldStyle(MyStyle())
                 .padding()
             Button("Veriyi Al"){
                 alinanVeri = textField
-            }
+            }.foregroundStyle(.white)
+                .padding()
+                .background(.pink)
+                .cornerRadius(3.0)
         }
         .padding()
+    }
+}
+
+struct MyStyle: TextFieldStyle{
+    func _body(configuration: TextField<Self._Label>) -> some View{
+        configuration
+            .padding()
+            .background(.green)
+            .cornerRadius(3.0)
+            .shadow(color: .gray, radius: 10)
+            .foregroundStyle(.white)
     }
 }
 
