9fda23090cdafb55f10aca6b3a6423cd47b18389
ercelserhat <ercelserhat@gmail.com>
2023-10-09 00:50:02 +0300

    DatePicker

---
+++ DateTimePicker/DateTimePicker/DateTimePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTimePickerApp.swift
+//  DateTimePicker
+//
+//  Created by Serhat on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DateTimePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
