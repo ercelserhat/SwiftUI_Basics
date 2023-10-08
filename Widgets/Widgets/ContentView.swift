becc48b891b7e3fe05921ed6c9b9509eb9cf2d72
ercelserhat <ercelserhat@gmail.com>
2023-10-08 23:43:30 +0300

    Widgets - Toggle

--- Widgets/Widgets/ContentView.swift
+++ Widgets/Widgets/ContentView.swift
@@ -9,32 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var resimAdi = "mutlu"
+    @State private var switchDurum = false
     
     var body: some View {
         VStack (spacing: 20){
-            Image(resimAdi).resizable().frame(width: 100, height: 100)
-            Button("Mutlu"){
-                resimAdi = "mutlu"
-            }
-            Button("Üzgün"){
-                resimAdi = "uzgun"
-            }
+            Toggle("Switch", isOn: $switchDurum)
+                .onChange(of: switchDurum, { oldValue, newValue in
+                    print("Switch Durum: \(newValue)")
+                })
+                .toggleStyle(SwitchToggleStyle(tint: .red))
+                .padding()
         }
         .padding()
     }
 }
 
-struct MyStyle: TextFieldStyle{
-    func _body(configuration: TextField<Self._Label>) -> some View{
-        configuration
-            .padding()
-            .background(.green)
-            .cornerRadius(3.0)
-            .shadow(color: .gray, radius: 10)
-            .foregroundStyle(.white)
-    }
-}
 
 #Preview {
     ContentView()
