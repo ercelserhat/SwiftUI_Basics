a3dfa403a2c57dc98de65f9ce213e89b708de61f
ercelserhat <ercelserhat@gmail.com>
2023-10-13 22:51:52 +0300

    CoreData - Create Database

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Serhat on 13.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    ContentView()
+}
