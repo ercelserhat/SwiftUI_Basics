a0d324e86022d904912a99871b32fb84674aa9ee
ercelserhat <ercelserhat@gmail.com>
2023-10-13 22:54:31 +0300

    CoreData - Create DAO Class

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     
     var body: some View {
         Text("")
+            .onAppear(){
+                let kdao = KisilerDao()
+            }
     }
 }
 
---
+++ CoreDataProject/CoreDataProject/KisilerDao.swift
@@ -0,0 +1,13 @@
+//
+//  KisilerDao.swift
+//  CoreDataProject
+//
+//  Created by Serhat on 13.10.2023.
+//
+
+import Foundation
+import CoreData
+
+class KisilerDao{
+    let context = persistentContainer.viewContext
+}
