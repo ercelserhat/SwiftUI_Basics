1c1b62740789a78f0a4eb8f0445241e0b5aced25
ercelserhat <ercelserhat@gmail.com>
2023-10-14 16:40:04 +0300

    Web Service - CRUD Response Class

---
+++ WebService/WebService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebService
+//
+//  Created by Serhat on 14.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebService/WebService/WebServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceApp.swift
+//  WebService
+//
+//  Created by Serhat on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
