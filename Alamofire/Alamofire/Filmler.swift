27aa0e12674b60a73a614cda5b5d48de4386f09b
ercelserhat <ercelserhat@gmail.com>
2023-10-14 20:25:12 +0300

    Alamofire - Read Example-3

---
+++ Alamofire/Alamofire/Filmler.swift
@@ -0,0 +1,8 @@
+//
+//  Filmler.swift
+//  AlamofireProject
+//
+//  Created by Serhat on 14.10.2023.
+//
+
+import Foundation
---
+++ Alamofire/Alamofire/FilmlerCevap.swift
@@ -0,0 +1,8 @@
+//
+//  FilmlerCevap.swift
+//  AlamofireProject
+//
+//  Created by Serhat on 14.10.2023.
+//
+
+import Foundation
