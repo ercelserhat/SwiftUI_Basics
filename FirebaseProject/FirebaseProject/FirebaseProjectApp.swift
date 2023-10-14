ae7dc6241bd912ff433274d95cce35eee31d1f94
ercelserhat <ercelserhat@gmail.com>
2023-10-14 21:44:42 +0300

    FirebaseProject

---
+++ FirebaseProject/FirebaseProject/FirebaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProjectApp.swift
+//  FirebaseProject
+//
+//  Created by Serhat on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
