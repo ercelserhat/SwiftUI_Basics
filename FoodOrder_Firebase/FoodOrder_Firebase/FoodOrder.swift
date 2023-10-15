232a5d3f2bc892628ad3b4aa63bb0928bdb240b4
ercelserhat <ercelserhat@gmail.com>
2023-10-16 00:00:17 +0300

    Firebase - Simple Food Order Application

---
+++ FoodOrder_Firebase/FoodOrder_Firebase/FoodOrder.swift
@@ -0,0 +1,27 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Serhat on 11.10.2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
+@main
+struct FoodOrder: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            Anasayfa()
+        }
+    }
+}
