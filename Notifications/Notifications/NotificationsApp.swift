2a6c61dcaa3b6216a885259804c82c2bb94647d4
ercelserhat <ercelserhat@gmail.com>
2023-10-16 01:08:14 +0300

    Notifications - Foreground notification

--- Notifications/Notifications/NotificationsApp.swift
+++ Notifications/Notifications/NotificationsApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct NotificationsApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
     }
 }
