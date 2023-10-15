b674169a38ec575fed70e90ef79aa2e8a037ebf4
ercelserhat <ercelserhat@gmail.com>
2023-10-16 01:37:17 +0300

    MapKit Framework - Add Pin

--- MapKitProject/MapKitProject/ContentView.swift
+++ MapKitProject/MapKitProject/ContentView.swift
@@ -10,13 +10,25 @@ import MapKit
 
 //39.9229566,32.8460175
 
+struct Pin: Identifiable{
+    let id = UUID()
+    let konum: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
     
     @State private var bolge = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.9229566, longitude: 32.8460175), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
     
+    let pinler = [
+        Pin(konum: CLLocationCoordinate2D(latitude: 39.9229566, longitude: 32.8460175)),
+        Pin(konum: CLLocationCoordinate2D(latitude: 41.0370323, longitude: 28.9644493))
+    ]
+    
     var body: some View {
         VStack(spacing: 30) {
-            Map(coordinateRegion: $bolge).frame(width: 300, height: 300)
+            Map(coordinateRegion: $bolge, annotationItems: pinler){pin in
+                MapMarker(coordinate: pin.konum)
+            }.frame(width: 300, height: 300)
             Button("Konuma Git"){
                 withAnimation{
                     bolge.center = CLLocationCoordinate2D(latitude: 41.0370323, longitude: 28.9644493)
@@ -24,7 +36,6 @@ struct ContentView: View {
                 }
             }
         }
-        
     }
 }
 
