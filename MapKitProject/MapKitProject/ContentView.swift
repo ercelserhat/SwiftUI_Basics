f6dfa5d761cc866e0765c5e8f8fa0ae315d64838
ercelserhat <ercelserhat@gmail.com>
2023-10-16 01:33:53 +0300

    MapKit Framework

---
+++ MapKitProject/MapKitProject/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  MapKitProject
+//
+//  Created by Serhat on 16.10.2023.
+//
+
+import SwiftUI
+import MapKit
+
+//39.9229566,32.8460175
+
+struct ContentView: View {
+    
+    @State private var bolge = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.9229566, longitude: 32.8460175), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Map(coordinateRegion: $bolge).frame(width: 300, height: 300)
+            Button("Konuma Git"){
+                withAnimation{
+                    bolge.center = CLLocationCoordinate2D(latitude: 41.0370323, longitude: 28.9644493)
+                    bolge.span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitProject/MapKitProject/MapKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitProjectApp.swift
+//  MapKitProject
+//
+//  Created by Serhat on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
